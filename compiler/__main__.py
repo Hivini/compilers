5d1cca4b7e1d9c0d09101b2ddb7d8fc5fad69c62
Vinicio Quintero <vinicioquinterobuss@gmail.com>
2021-11-06 15:58:50 -0700

    Added a simple logger to display messages in console

--- compiler/__main__.py
+++ compiler/__main__.py
@@ -2,16 +2,23 @@ import argparse
 import sys
 import os
 
-def run():
+from .logger import Logger
+
+
+def Run():
+    # Create logger
+    logger = Logger()
+
     # Parse arguments
     parser = argparse.ArgumentParser()
     parser.add_argument(
         "file_path", help="Location of the file to compile, relative to current location.")
     args = parser.parse_args()
     file_path = args.file_path
+
     # Open file
-    if not os.path.isfile(file_path):    
-        print(f'{file_path} does not exist or is not a file.')
+    if not os.path.isfile(file_path):
+        logger.LogError(f'{file_path} does not exist or is not a file.')
         sys.exit(1)
     try:
         f = open(file_path, 'r')
@@ -19,9 +26,11 @@ def run():
         print(program)
         f.close()
     except FileNotFoundError:
-        print(f'{file_path} could not be opened!')
+        logger.LogError(f'{file_path} could not be opened!')
         sys.exit(1)
+
+    logger.LogSuccess('Successfully compiled!')
 
 
 if __name__ == '__main__':
-    run()
+    Run()
