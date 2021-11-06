8584704aae753775f36d7028028f40168ec68cc8
Vinicio Quintero <vinicioquinterobuss@gmail.com>
2021-11-06 15:44:53 -0700

    Added program flag to determine file to be opened

--- compiler/__main__.py
+++ compiler/__main__.py
@@ -1,18 +1,26 @@
+import argparse
+import sys
 import os
 
-FILE_NAME = 'code2.txt'
-
 def run():
-    if (os.path.isfile(FILE_NAME)):    
-        try:
-            f = open(FILE_NAME, 'r')
-            program = f.read()
-            print(program)
-            f.close()
-        except FileNotFoundError:
-            print('File could not be opened!')
-    else:
-        print(f'{FILE_NAME} does not exist or is not a file.')
+    # Parse arguments
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "file_path", help="Location of the file to compile, relative to current location.")
+    args = parser.parse_args()
+    file_path = args.file_path
+    # Open file
+    if not os.path.isfile(file_path):    
+        print(f'{file_path} does not exist or is not a file.')
+        sys.exit(1)
+    try:
+        f = open(file_path, 'r')
+        program = f.read()
+        print(program)
+        f.close()
+    except FileNotFoundError:
+        print(f'{file_path} could not be opened!')
+        sys.exit(1)
 
 
 if __name__ == '__main__':
