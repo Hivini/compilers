652fef5b07e99aa7e33d63196d81d6899e675de0
Vinicio Quintero <vinicioquinterobuss@gmail.com>
2021-11-06 15:39:09 -0700

    Added simple mechanism to open files

--- compiler/__main__.py
+++ compiler/__main__.py
@@ -1,5 +1,19 @@
-def run():
-    print('Hello World!')
+import os
 
-if __name__ == "__main__":
+FILE_NAME = 'code2.txt'
+
+def run():
+    if (os.path.isfile(FILE_NAME)):    
+        try:
+            f = open(FILE_NAME, 'r')
+            program = f.read()
+            print(program)
+            f.close()
+        except FileNotFoundError:
+            print('File could not be opened!')
+    else:
+        print(f'{FILE_NAME} does not exist or is not a file.')
+
+
+if __name__ == '__main__':
     run()
