d76af1e3c0ce19b4b7ff07bdc4d576ab90f8efcb
Vinicio Quintero <vinicioquinterobuss@gmail.com>
2021-11-06 17:11:52 -0700

    Adapted the lexer the professor provided into a class called LexerManager

---
+++ compiler/lexer.py
@@ -0,0 +1,58 @@
+import ply.lex as lex
+
+from enum import Enum
+
+
+class LexerTypes(Enum):
+    INTNUM = 1
+    FLOATNUM = 2
+    NAME = 3
+    INTDCL = 4
+    FLOATDCL = 5
+    PRINT = 6
+
+
+class LexerManager:
+    # List of literals to avoid writing simple regexp for each one.
+    literals = ['+', '-', '*', '/', '(', ')', '=']
+    # Reserved keywords
+    reserved = {
+        'int': LexerTypes.INTDCL.name,
+        'float': LexerTypes.FLOATDCL.name,
+        'print': LexerTypes.PRINT.name
+    }
+    # Tokens that determine the functions
+    tokens = [
+        LexerTypes.INTNUM.name,
+        LexerTypes.FLOATNUM.name,
+        LexerTypes.NAME.name
+    ] + list(reserved.values())
+    # A string containing ignored characters (spaces and tabs)
+    t_ignore = ' \t'
+
+    def t_NAME(self, t):
+        r'[a-zA-Z_][a-zA-Z_0-9]*'
+        t.type = self.reserved.get(t.value, LexerTypes.NAME.name)
+        return t
+
+    def t_FNUMBER(self, t):
+        r'\d+\.\d+'
+        t.value = float(t.value)
+        return t
+
+    def t_INUMBER(self, t):
+        r'\d+'
+        t.value = int(t.value)
+        return t
+
+    def t_newline(self, t):
+        r'\n+'
+        t.lexer.lineno += t.value.count("\n")
+
+    def t_error(self, t):
+        print("Illegal character '%s'" % t.value[0])
+        t.lexer.skip(1)
+
+    def createLexer(self):
+        lexer = lex.lex(module=self)
+        return lexer
